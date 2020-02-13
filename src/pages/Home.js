58827f80ed412d1c3e0064b5fae79f46b164a773
lukmansjy <lukman.rocks@live.com>
2020-02-13 10:22:15 +0700

    2.Login

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,26 @@
 import React, {Component} from 'react'
+import {Modal, Form } from 'react-bootstrap'
 
 class Home extends Component{
+    constructor(){
+        super()
+        this.state = {
+            modalLogin: false,
+            modalRegister: false
+        }
+    }
+
+    handleShowLogin = ()=>{
+        this.setState({
+            modalLogin: true
+        })
+    }
+    handleHideLogin = ()=>{
+        this.setState({
+            modalLogin: false
+        })
+    }
+
     render(){
         return(
             <div className="homeContain">
@@ -10,7 +30,7 @@ class Home extends Component{
                             <img src={require('../../src/assets/icons/cats-couple-love.png')} alt="BreedNder Logo" />
                             <span>BreedNder</span>
                         </div>
-                        <button className="myButton">Login</button>
+                        <button className="myButton" onClick={this.handleShowLogin}>Login</button>
                     </div>
                     <div className="msg">
                         <div>
@@ -29,6 +49,20 @@ class Home extends Component{
                 <div className="homeFotter">
                     <p>FIND YOUR PET'S MATCH</p>
                 </div>
+
+                {/* ###### MODAL ###### */}
+                {/* MODAL LOGIN */}
+                <Modal show={this.state.modalLogin} onHide={this.handleHideLogin} className="modalContent">
+                    <div>
+                        <span className="modalTitle">Login</span>
+                        <button className="btnModalClose" onClick={this.handleHideLogin}>X</button>
+                    </div>
+                    <div className="formModal">
+                        <Form.Control type="email" placeholder="Email" />
+                        <Form.Control type="password" placeholder="Password" />
+                        <button onClick={this.handleBtnLogin} className="btnRegister">Login</button>
+                    </div>
+                </Modal>
                 
             </div>
         )
