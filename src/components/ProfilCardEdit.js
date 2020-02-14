c514495a63fa0f3eef9246840f04614cbfded7a4
lukmansjy <lukman.rocks@live.com>
2020-02-14 15:16:38 +0700

    add input edit profil

--- src/components/ProfilCardEdit.js
+++ src/components/ProfilCardEdit.js
@@ -32,12 +32,20 @@ const ProfilCardEdit = (props)=>{
                 <h6>Name Pet</h6>
                 <input className="inputText" type="text"/>
                 <h6>Gender</h6>
-                <select className="myInputSelect mt-8">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
+                <select className="myInputSelect mt-8 mb-12">
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
                 </select>
+                <h6>Age</h6>
+                <select className="myInputSelect mt-8 mb-12">
+                    <option value="Child">Child</option>
+                    <option value="Teenager">Teenager</option>
+                    <option value="Adult">Adult</option>
+                </select>
+                <h6>About Pet</h6>
+                <textarea rows="3"></textarea>
                 <div className="containerBtnSave">
-                    <div className="contentMyBtnMini mb-12">
+                    <div className="contentMyBtnMini mb-20">
                         <button className="myBtnMini" onClick={props.handleSave}>Save</button>
                     </div>
                 </div>
